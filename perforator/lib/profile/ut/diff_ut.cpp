52a69a6082a11eb2498f10b5552d744725217e70
sskvor <sskvor@yandex-team.com>
2025-07-17 20:29:44 +0300

    [perforator] Add test on profile merging commit_hash:1d45d91fb04dd800efcca22ad6557ec56c2ceaa2

--- perforator/lib/profile/ut/diff_ut.cpp
+++ perforator/lib/profile/ut/diff_ut.cpp
@@ -1,3 +1,5 @@
+#include "golden.h"
+
 #include <perforator/lib/profile/flat_diffable.h>
 #include <perforator/lib/profile/pprof.h>
 #include <perforator/lib/profile/profile.h>
@@ -5,62 +7,8 @@
 #include <library/cpp/testing/gtest/gtest.h>
 #include <library/cpp/testing/common/env.h>
 
-#include <util/folder/path.h>
-#include <util/stream/file.h>
-#include <util/stream/zlib.h>
 
-
-static TVector<TFsPath> ListGoldenProfiles() {
-    TFsPath dir = SRC_("testprofiles");
-    TVector<TFsPath> children;
-    dir.List(children);
-    Y_ENSURE(!children.empty());
-    return children;
-}
-
-static TString SerializeFlatProfile(const NPerforator::NProfile::TFlatDiffableProfile& profile) {
-    TStringStream out;
-    profile.WriteTo(out);
-    return out.Str();
-}
-
-static TMap<TString, ui64> CountFlatProfileEvents(const NPerforator::NProfile::TFlatDiffableProfile& profile) {
-    TMap<TString, ui64> sum;
-
-    profile.IterateSamples([&](TStringBuf, const TMap<TString, ui64>& values) {
-        for (auto& [type, value] : values) {
-            sum[type] += value;
-        }
-    });
-
-    return sum;
-}
-
-template <typename L, typename R>
-static void CompareFlatProfiles(L&& lhs, R&& rhs) {
-    // Our profiles a somewhat malformed
-    NPerforator::NProfile::TFlatDiffableProfileOptions options{
-        .LabelBlacklist = {"comm"},
-    };
-    NPerforator::NProfile::TFlatDiffableProfile left{lhs, options};
-    NPerforator::NProfile::TFlatDiffableProfile right{rhs, options};
-
-    auto lhsEvents = CountFlatProfileEvents(left);
-    auto rhsEvents = CountFlatProfileEvents(right);
-    EXPECT_EQ(lhsEvents, rhsEvents);
-
-    TString pprofString = SerializeFlatProfile(left);
-    TString protoString = SerializeFlatProfile(right);
-    EXPECT_EQ(pprofString, protoString);
-}
-
-static NPerforator::NProto::NPProf::Profile ParsePprof(const TFsPath& path) {
-    TFileInput serialized{path};
-    TZLibDecompress uncompressed{&serialized};
-    NPerforator::NProto::NPProf::Profile proto;
-    Y_ENSURE(proto.ParseFromArcadiaStream(&uncompressed));
-    return proto;
-}
+using namespace NPerforator::NProfile::NTest;
 
 // NOLINTNEXTLINE(readability-identifier-naming)
 struct GoldenProfileTest : testing::TestWithParam<TFsPath> {};
@@ -91,5 +39,5 @@ TEST_P(GoldenProfileTest, ConvertPprofRoundTripCanon) {
 INSTANTIATE_TEST_SUITE_P(
     GoldenProfiles,
     GoldenProfileTest,
-    testing::ValuesIn(ListGoldenProfiles())
+    testing::ValuesIn(NPerforator::NProfile::NTest::ListGoldenProfiles("diff", 5))
 );
