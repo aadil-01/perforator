c3cc5699637efbd23137ca2e46fdb539a13d0978
akaretnikov <akaretnikov@yandex-team.com>
2025-08-05 12:47:12 +0300

    fix jumping animation in select commit_hash:63cb7955abc7073644c6e5d38064fad63e0daa8a

--- perforator/ui/app/src/components/Select/Select.tsx
+++ perforator/ui/app/src/components/Select/Select.tsx
@@ -113,7 +113,7 @@ export const Select: React.FC<SelectProps> = props => {
                     />
                 </div>
             )}
-            controlClassName={b('control')}
+            className={b('control')}
             popupClassName={b('popup')}
             // controls loader in the end of the list in select
             loading={hasMore || loadState === 'loading'}
